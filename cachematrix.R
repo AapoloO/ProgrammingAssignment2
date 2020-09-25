4e2375f5cc194bcf9e5c7dbf32227249a24bf113
AapoloO <70042911+AapoloO@users.noreply.github.com>
2020-09-25 18:57:24 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function take a basic matrix created in R
+## Then create a lot of other function inside the environment
+## so it can be calculated the inverse in each part
+## the return of the first function is a list that contain the other functions
+## so i can execute a function of the makeCacheMatrix environment from assign
+## the function to a variable and select from the $ symbol
 
-## Write a short comment describing this function
+## With the second function it is calculated the inverse of elements that
+## has been assigned to the previous list
+
+## This function generates an environment with other functions that can be 
+## execute from other function, for that is that is called 
+## "makeCacheMatrix" unless it´s return is a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver <- NULL  
+  set <- function(y){ 
+    x <<- y 
+    inver <<- NULL}
+  get <- function() x 
+  setinver <- function(inverse) inver <<- solve 
+  getinver <- function() inver 
+  #all this function are in the environment of makeCacheMatrix
+  return(list(set = set, get = get, 
+              setinver = setinver,
+              getinver = getinver))
 }
 
+##This function execute the segments of the previous list
+##and return the inverse value of the matrix catched
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {        inver <- x$getinver() 
+#if some data is in inver then the matrix inverse is already done
+if(!is.null(inver)) { 
+  me<-message("getting cached data") 
+  return(me)
 }
+#this function will execute the solve function to the matrix
+matrixin <- x$get() 
+inver <- solve(matrixin, ...) 
+x$setinver(inver) 
+return(inver)
+        
+}
+
+                
